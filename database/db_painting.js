7b1ea52356b94ecb806ceb36427c506adb15e6ea
Kay Kim <kimjh@bawi.org>
2020-05-18 01:08:57 +0900

    add 'withProducts' property at req for getting painting info

--- database/db_painting.js
+++ database/db_painting.js
@@ -7,7 +7,7 @@ const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../status_code');
 
 const getPainting = async (req, res, next) => {
   winston.info('getPainting called!');
-  const { id } = req.body;
+  const { id, withProducts } = req.body;
   if (id === undefined || id === null) {
     const error = new Error('400 Bad Request');
     error.status = HTTP_STATUS_CODE.BAD_REQUEST;
@@ -15,14 +15,18 @@ const getPainting = async (req, res, next) => {
     return next(error);
   }
   try {
-    const data = await Painting.findOne({
-      attributes: ['name', 'image_src', 'content', 'num_like', 'view', 'userId'],
+    const painting = await Painting.findOne({
+      attributes: ['id', 'name', 'image_src', 'content', 'num_like', 'view', 'userId'],
       where: { id: id }
     });
-    if (data === null) {
+    if (painting == null) {
       return res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({ error: DB_STATUS_CODE.NO_SUCH_PAINTING });
-    }
-    return res.status(HTTP_STATUS_CODE.OK).json({ data: data, error: DB_STATUS_CODE.OK });
+    };
+    if (withProducts) {
+      const products = await painting.getProducts();
+      return res.status(HTTP_STATUS_CODE.OK).json({ painting, products, error: DB_STATUS_CODE.OK });
+    };
+    return res.status(HTTP_STATUS_CODE.OK).json({ painting, error: DB_STATUS_CODE.OK });
   } catch (error) {
     winston.error(`getPaintingError: ${error}`);
     return next(error);
@@ -34,9 +38,9 @@ const setPainting = async (req, res, next) => {
   const { username, name, image_src, content } = req.body;
   try {
     const user = await User.findOne({ where: { username: username } });
-    if (user === null) {
+    if (user == null) {
       return res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({ error: DB_STATUS_CODE.NO_SUCH_USER });
-    }
+    };
     await Painting.create({
       name,
       image_src,
@@ -46,7 +50,6 @@ const setPainting = async (req, res, next) => {
     return res.status(HTTP_STATUS_CODE.CREATED).json({ error: DB_STATUS_CODE.OK });
   } catch (error) {
     winston.error(`setPaintingError: ${error}`);
-
     return next(error);
   };
 };
