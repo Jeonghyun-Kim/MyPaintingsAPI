2da7555a70a532e498189aaf27ed13225b62d69e
Kay Kim <kimjh@bawi.org>
2020-05-17 19:34:10 +0900

    add db_product api (get, post) /product

---
+++ database/db_product.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const crypto = require('crypto');
+const { Painting, Product } = require('../models');
+const winston = require('../winston_config');
+const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../status_code');
+
+const getProduct = async (req, res, next) => {
+  winston.info('getProduct called!');
+  const { id } = req.body;
+  if (id === undefined || id === null) {
+    const error = new Error('400 Bad Request');
+    error.status = HTTP_STATUS_CODE.BAD_REQUEST;
+    winston.error(`getProductError: ${error}`);
+    return next(error);
+  }
+  try {
+    const data = await Product.findOne({
+      attributes: ['name', 'image_src', 'content', 'price', 'on_sale', 'num_like', 'view', 'paintintId'],
+      where: { id: id }
+    });
+    if (data === null) {
+      return res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({ error: DB_STATUS_CODE.NO_SUCH_PRODUCT });
+    }
+    return res.status(HTTP_STATUS_CODE.OK).json({ data: data, error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    winston.error(`getProductError: ${error}`);
+    return next(error);
+  };
+};
+
+const setProduct = async (req, res, next) => {
+  winston.info('setProduct called!');
+  const { paintingId, name, image_src, content, price, on_sale } = req.body;
+  try {
+    if (await Painting.findOne({ where: { id: paintingId } }) === null) {
+      return res.status(HTTP_STATUS_CODE.BAD_REQUEST).json({ error: DB_STATUS_CODE.NO_SUCH_PAINTING });
+    }
+    await Product.create({
+      name,
+      image_src,
+      content,
+      price,
+      on_sale,
+      painttingId: paintingId
+    });
+    return res.status(HTTP_STATUS_CODE.CREATED).json({ error: DB_STATUS_CODE.OK });
+  } catch (error) {
+    winston.error(`setProductError: ${error}`);
+
+    return next(error);
+  };
+};
+
+module.exports = {
+  getProduct,
+  setProduct
+};
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const { HTTP_STATUS_CODE, DB_STATUS_CODE } = require('../status_code');
 const winston = require('../winston_config');
 const { getUser, setUser } = require('../database/db_user');
 const { getPainting, setPainting } = require('../database/db_painting');
+const { getProduct, setProduct } = require('../database/db_product');
 
 // const React = require('react');
 // require('babel-register')({
@@ -32,5 +33,9 @@ router.route('/user')
 router.route('/painting')
   .get(getPainting)
   .post(setPainting)
+
+router.route('/product')
+  .get(getProduct)
+  .post(setProduct)
 
 module.exports = router;
